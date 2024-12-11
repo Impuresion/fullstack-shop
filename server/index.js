7b75970c4a34244227897b140a5debacba9fdc97
Impuresion <johnyzlatan@gmail.com>
2024-12-11 16:29:06 +0300

    progress

--- server/index.js
+++ server/index.js
@@ -2,8 +2,16 @@ require('dotenv').config();
 
 const express = require('express');
 const sequelize = require('./db');
+const models = require('./models/models');
+const cors = require('cors');
 const app = express();
+
+app.use(cors());
+app.use(express.json());
+
 const PORT = process.env.PORT || 5000;
+
+
 const start = async ()=>{
     try{
         await sequelize.authenticate()
@@ -13,5 +21,4 @@ const start = async ()=>{
         console.log(e);
     }
 }
-
 start()
